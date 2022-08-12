43792a0336fbd8fb7d443f58e279351b866651ea
MichealTene7 <110479175+MichealTene7@users.noreply.github.com>
2022-08-12 17:55:07 -0400

    add 'as an Rscript' to differentiate from manual

--- src/loc/thereAndBackAgain.R
+++ src/loc/thereAndBackAgain.R
@@ -3,9 +3,9 @@ gapminder = read.csv("data/gapminder_data.csv")
 gapminderAmerica = gapminder[gapminder$continent == "Americas",]
 file.create("output/thereAndBackAgain.pdf")
 pdf("output/thereAndBackAgain.pdf")
-plot(y = gapminder$gdpPercap, x = gapminder$year, type ="l", main = "There and Back Again")
+plot(y = gapminder$gdpPercap, x = gapminder$year, type ="l", main = "There and Back Again as an RScript")
 ggplot(data = gapminderAmerica, mapping = aes(x = year, y = gdpPercap, color = country)) + 
   geom_line(show.legend = FALSE)+
-  ggtitle("There and Back Again Package")
+  ggtitle("There and Back Again Package as an RScript")
 dev.off()
 
