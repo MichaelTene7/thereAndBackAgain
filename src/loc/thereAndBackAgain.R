e3342b317578b2a4e3628289cb846954d51ac991
MichealTene7 <110479175+MichealTene7@users.noreply.github.com>
2022-08-12 17:47:11 -0400

    added pdf dev

--- src/loc/thereAndBackAgain.R
+++ src/loc/thereAndBackAgain.R
@@ -1,9 +1,11 @@
-install.packages("ggplot2")
 library(ggplot2)
 gapminder = read.csv("data/gapminder_data.csv")
 gapminderAmerica = gapminder[gapminder$continent == "Americas",]
+file.create("output/thereAndBackAgain.pdf")
+pdf("output/thereAndBackAgain.pdf")
 plot(y = gapminder$gdpPercap, x = gapminder$year, type ="l", main = "There and Back Again")
 ggplot(data = gapminderAmerica, mapping = aes(x = year, y = gdpPercap, color = country)) + 
   geom_line(show.legend = FALSE)+
   ggtitle("There and Back Again Package")
+dev.off()
 
